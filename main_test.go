3ab16999b41ec2fe05802b58eeee526b8dd6a3fa
Kelmik91 <kelmik91@me.com>
2023-08-18 01:08:48 +0300

    rename package

--- main_test.go
+++ main_test.go
@@ -1,4 +1,4 @@
-package main
+package weather
 
 import "testing"
 
@@ -7,7 +7,7 @@ func TestWeather(t *testing.T) {
 	latitude := 55.6302
 	longitude := 37.6045
 	forecastDays := "1"
-	
+
 	t.Run("weather", func(t *testing.T) {
 		message, err := Weather(latitude, longitude, forecastDays)
 		if err != nil {
